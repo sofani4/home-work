1de2b4d8a4cdedef9bbd9240f207bd459c0d9951
Sofi <sofani4@gmail.com>
2021-11-18 20:58:21 +0500

    Задание №3 Правки для pull request

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -1,9 +1,6 @@
 package com.sbrf.reboot;
 
 
-import com.sbrf.reboot.service.Account;
-import com.sbrf.reboot.service.AccountRepository;
-
 
 public class Main {
     public static void main(String[] args) {
